e7abecd712d17443e522042bed738e8c8644c9e4
MukilanRajaK <kmukilanraja@gmail.com>
2020-08-26 08:57:33 +0530

    credentials removed

--- DBCONFIG.php
+++ DBCONFIG.php
@@ -1,9 +1,4 @@
 <?php
-/****************************************************/
-/************** Created by : Vivek Gupta ************/
-/***************     www.vsgupta.in     *************/
-/***************     www.iotmonk.com     *************/
-/****************************************************/ 
 define('DB_USER', "Your database user name");     // Your database user name
 define('DB_PASSWORD', "Your database password");			// Your database password (mention your db password here)
 define('DB_DATABASE', "Your database name"); // Your database name
